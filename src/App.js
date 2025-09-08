028b16368fe68eeef99b2e93de76c2c30cfa54e2
GrockTech <ootiarhin@yahoo.com>
2025-09-08 17:23:04 +0000

    netlify-fix

--- src/App.js
+++ src/App.js
@@ -1,32 +1,64 @@
+// // App.js
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // includes Popper
+// import './App.css';
+
+// import Navbar from '../src/components/Navbar';
+// import HeroSection from '../src/components/Hero';
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import AdminLogin from './components/AdminLogin';
+// import Dashboardform from './components/Adminshipmentform';
+// import ShipmentList from './components/ShipmentList';
+// function App() {
+//   return (
+//     <div className="App container" id='animate'>
+//       <Router>
+//         {/* Navbar is shown on all pages */}
+//         <Navbar />
+
+//         {/* Routes control what else shows */}
+//         <Routes>
+//           {/* Homepage */}
+//           <Route path="/" element={<HeroSection />} />
+
+//           {/* Admin login page */}
+//           <Route path="/admin" element={<AdminLogin />} />
+//           <Route path="/dashboard" element={<Dashboardform />} />
+//           <Route path="/listshipments" element={<ShipmentList />} />
+//         </Routes>
+//       </Router>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
 // App.js
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // includes Popper
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './App.css';
 
-import Navbar from '../src/components/Navbar';
-import HeroSection from '../src/components/Hero';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import HeroSection from './components/Hero';
+import { Routes, Route } from "react-router-dom";
 import AdminLogin from './components/AdminLogin';
 import Dashboardform from './components/Adminshipmentform';
 import ShipmentList from './components/ShipmentList';
+
 function App() {
   return (
     <div className="App container" id='animate'>
-      <Router>
-        {/* Navbar is shown on all pages */}
-        <Navbar />
+      {/* Navbar is shown on all pages */}
+      <Navbar />
 
-        {/* Routes control what else shows */}
-        <Routes>
-          {/* Homepage */}
-          <Route path="/" element={<HeroSection />} />
-
-          {/* Admin login page */}
-          <Route path="/admin" element={<AdminLogin />} />
-          <Route path="/dashboard" element={<Dashboardform />} />
-          <Route path="/listshipments" element={<ShipmentList />} />
-        </Routes>
-      </Router>
+      {/* Routes control what else shows */}
+      <Routes>
+        <Route path="/" element={<HeroSection />} />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/dashboard" element={<Dashboardform />} />
+        <Route path="/listshipments" element={<ShipmentList />} />
+      </Routes>
     </div>
   );
 }
